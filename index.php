e2bedc10f61f36f7b668d4b7319b2b918ead0e23
Manuel Urak <manuel.urak88@gmail.com>
2020-07-27 17:48:12 +0200

    made changes

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 	session_start();
 	require('PHP/registration-connection.php');
 	require('PHP/adminpanel-components.php');
-	require('PHP/operation.php');
-	
+	require('PHP/admin-operation.php');
+
 	if(!isset($_SESSION['user']))
 	{
 		header('location: login.php');
@@ -17,9 +17,10 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>Adminpanel</title>
+	<title>Home</title>
 	<script src="https://kit.fontawesome.com/969b0d4b22.js" crossorigin="anonymous"></script>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="Stylesheets/style.css">
 </head>
 <body>
 	<nav class="navbar navbar-expand-lg navbar-light bg-dark">
@@ -37,50 +38,56 @@
       			<li class="nav-item">
         			<a class="nav-link text-light" href="register.php">Register</a>
       			</li>
-      			<li class="nav-item">
-        			<a class="nav-link text-light" href="userinfo.php">Profile</a>
-      			</li>
     		</ul>
  	 	</div>
 	</nav>
 	<main>
-		<div class="d-flex justify-content-center pt-4">
-			<?php  
-				buttonElement($btnid = "btn-read", $styleclass = "btn btn-primary", $text = "Browse Our Animal Database", $name = "read", $attr = "dat-toggle='tooltip' data-placement='bottom' title='Read'");
-			?>
-		</div>
-		<div class="container d-flex">
-			<?php  
-				if(isset($_POST['read']))
+		<div class="container text-center">
+			<div class="d-flex justify-content-center pt-4 pb-4">
+				<form method="post" class="w-50">
+					<div class="d-flex justify-content-center">
+						<?php  
+
+							buttonElement($btnid = "btn-read", $styleclass = "btn btn-primary", $text = "Browse our animal database", $name = "read", $attr = "dat-toggle='tooltip' data-placement='bottom' title='Read'");
+							
+						?>
+					</div>
+				</form>
+			</div>
+			<div class="d-flex flex-wrap justify-content-center">
+				<?php  
+
+					if(isset($_POST['read']))
+					{
+						$result = getData();
+
+						if($result)
+						{
+							while($row = mysqli_fetch_assoc($result))
 							{
-								$result = getData();
+								?>
+											
+								<div class="card m-2 bg-dark" style="width: 25%;">
+  									<img class="card-img-top" src="<?php echo $row['animal_image']; ?>" style="width: 250px; height: 250px;">
+  									<div class="card-body bg-dark">
+    									<h5 class="card-title text-light"><?php echo $row['animal_name']; ?></h5>
+  									</div>
+  									<ul class="list-group list-group-flush bg-dark">
+									    <li class="list-group-item">Type :<?php echo $row['animal_type']; ?></li>
+									    <li class="list-group-item">Age: <?php echo $row['animal_age']; ?></li>
+									    <li class="list-group-item">ZIP: <?php echo $row['zipcode']; ?></li>
+									    <li class="list-group-item">Location: <?php echo $row['location']; ?></li>
+									    <li class="list-group-item"><i class="fas fa-phone"></i><?php echo $row['phone_number']; ?></li>
+									</ul>
+								</div>
 
-								if($result)
-								{
-									while($row = mysqli_fetch_assoc($result))
-									{
-										?>
-
-										<div class="card">
-  											<img class="card-img-top" src="<?php echo $row['animal_image']; ?>" alt="Card image cap">
-  											<div class="card-body">
-    											<h5 class="card-title"><?php echo $row['animal_name']; ?></h5>
-  											</div>
-  											<ul class="list-group list-group-flush">
-  												<li class="list-group-item"><?php echo $row['animalID']; ?></li>
-    											<li class="list-group-item"><?php echo $row['zipcode']; echo $row['location'];?></li>
-    											<li class="list-group-item"><?php echo $row['animal_type']; ?></li>
-    											<li class="list-group-item"><?php echo $row['animal_age']; ?></li>
-    											<li class="list-group-item"><?php echo $row['phonenumber']; ?></li>
-  											</ul>
-										</div>
-
-										<?php
-									}
-								}
+								<?php
 							}
+						}
+					}
 
-			?>
+				?>		
+			</div>
 		</div>
 	</main>
 
